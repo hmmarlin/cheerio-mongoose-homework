85881e389523ab60749eebc5e1592a5a4e9742c2
Hannah Marlin <hmmarlin@gmail.com>
2016-11-05 10:57:51 -0700

    update server.js file for heroku

--- server.js
+++ server.js
@@ -1,34 +1,41 @@
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var request = require('request'); 
+var request = require('request');
 var cheerio = require('cheerio');
 
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 
 app.use(express.static('public'));
 
 
-mongoose.connect('mongodb://localhost/headlines');
+// mongoose.connect('mongodb://localhost/headlines');
+
+
+var databaseUri = 'mongodb://localhost/headlines';
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect(databaseUri);
+}
+
 var db = mongoose.connection;
 
-
 db.on('error', function(err) {
-  console.log('Mongoose Error: ', err);
+    console.log('Mongoose Error: ', err);
 });
 
 
 db.once('open', function() {
-  console.log('Mongoose connection successful.');
+    console.log('Mongoose connection successful.');
 });
 
 
@@ -42,85 +49,81 @@ var Article = require('./models/Article.js');
 
 
 app.get('/', function(req, res) {
-  res.send(index.html);
+    res.send(index.html);
 });
 
 app.get('/scrape', function(req, res) {
-  request('http://www.theonion.com/section/local', function(error, response, html) {
-    var $ = cheerio.load(html);
-    $('div.info').each(function(i, element) {
+    request('http://www.theonion.com/section/local', function(error, response, html) {
+        var $ = cheerio.load(html);
+        $('div.info').each(function(i, element) {
 
-				var result = {};
-
-				
-				result.title = $(this).children('div.inner').children('header').children('h2.headline').children('a').text().trim();
-				var url = $(this).children('div.inner').children('header').children('h2.headline').children('a').attr('href').trim();
-				result.link = 'http://www.theonion.com' + url;
-				result.summary = $(this).children('div.inner').children('div.desc').text().trim();
-				result.image = $(this).children('figure.thumb').children('a').children('div.image').children('noscript').children('img').attr('src');
-							var entry = new Article (result);
-
-				entry.save(function(err, doc) {
-				  if (err) {
-				    console.log(err);
-				  } 
-				  else {
-				    console.log(doc);
-				  }
-				});
+            var result = {};
 
 
+            result.title = $(this).children('div.inner').children('header').children('h2.headline').children('a').text().trim();
+            var url = $(this).children('div.inner').children('header').children('h2.headline').children('a').attr('href').trim();
+            result.link = 'http://www.theonion.com' + url;
+            result.summary = $(this).children('div.inner').children('div.desc').text().trim();
+            result.image = $(this).children('figure.thumb').children('a').children('div.image').children('noscript').children('img').attr('src');
+            var entry = new Article(result);
+
+            entry.save(function(err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(doc);
+                }
+            });
+
+
+        });
     });
-  });
     res.redirect('/');
 });
 
-app.get('/articles', function(req, res){
-	Article.find({}, function(err, doc){
-		if (err){
-			console.log(err);
-		} 
-		else {
-			res.json(doc);
-		}
-	});
+app.get('/articles', function(req, res) {
+    Article.find({}, function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(doc);
+        }
+    });
 });
 
-app.get('/articles/:id', function(req, res){
-	
-	Article.findOne({'_id': req.params.id})
-	.populate('note')
-	.exec(function(err, doc){
-		if (err){
-			console.log(err);
-		} 
-		else {
-			res.json(doc);
-		}
-	});
+app.get('/articles/:id', function(req, res) {
+
+    Article.findOne({ '_id': req.params.id })
+        .populate('note')
+        .exec(function(err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(doc);
+            }
+        });
 });
 
 
 
-app.post('/articles/:id', function(req, res){
-	var newNote = new Note(req.body);
+app.post('/articles/:id', function(req, res) {
+    var newNote = new Note(req.body);
 
-	newNote.save(function(err, doc){
-		if(err){
-			console.log(err);
-		} 
-		else {
-		
-			Article.findOneAndUpdate({'_id': req.params.id}, {'note':doc._id})
-			.exec(function(err, doc){
-				if (err){
-					console.log(err);
-				} else {
-					res.send(doc);
-				}
-			});
-		}
-	});
+    newNote.save(function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+
+            Article.findOneAndUpdate({ '_id': req.params.id }, { 'note': doc._id })
+                .exec(function(err, doc) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.send(doc);
+                    }
+                });
+        }
+    });
 });
 
 
@@ -131,5 +134,5 @@ app.post('/articles/:id', function(req, res){
 
 // listen on port 3000
 app.listen(3000, function() {
-  console.log('App running on port 3000!');
+    console.log('App running on port 3000!');
 });
